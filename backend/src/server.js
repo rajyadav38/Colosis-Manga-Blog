1820fd0d968a643899316544cb1edc628ce9c7cb
rajyadav38 <rajyadav5566788@gmail.com>
2026-03-06 13:02:55 +0530

    Fixed backend merge conflicts and server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,13 @@
 require("dotenv").config();
 const app = require("./app");
-<<<<<<< HEAD
 
-// 🔹 MongoDB
+// MongoDB
 const connectMongoDB = require("./config/mongo");
 const PORT = process.env.PORT || 5000;
 
 // Connect MongoDB FIRST
 connectMongoDB();
 
-=======
-const PORT = process.env.PORT || 5000;
-
->>>>>>> f10666a6706c0f42b76b8af4a7e6d2ea3fb63dd5
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
