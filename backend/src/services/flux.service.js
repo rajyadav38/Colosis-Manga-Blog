0b23587dfd46643d5eb13299f1439efeda6cd7cd
rajyadav38 <rajyadav5566788@gmail.com>
2026-06-26 14:49:01 +0530

    model change for testing

--- backend/src/services/flux.service.js
+++ backend/src/services/flux.service.js
@@ -3,8 +3,9 @@ const axios = require("axios");
 const generateImage = async (prompt) => {
   try {
     console.log("Calling HuggingFace...");
+
     const response = await axios({
-      url: "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev",
+      url: "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
       method: "POST",
       headers: {
         Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
@@ -15,11 +16,11 @@ const generateImage = async (prompt) => {
       },
       responseType: "arraybuffer",
     });
-    console.log("Response received.");
 
     return Buffer.from(response.data);
   } catch (error) {
-    console.log("Flux Error:", error.response?.data || error.message);
+    console.log("HF Error:", error.response?.data?.toString() || error.message);
+
     throw error;
   }
 };
